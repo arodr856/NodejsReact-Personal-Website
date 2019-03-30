b5b981d93da3375b8f62de5ed71e7a62699da417
arodr856 <arodr856@gmail.com>
2019-03-30 15:15:03 -0700

    changed text color for navbar items

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -33,36 +33,36 @@ export default class NavBar extends React.Component {
     render() {
         return (
         <div>
-            <Navbar expand="md">
-            <NavbarBrand href="/">Alex Rodriguez</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                <NavItem>
-                    <NavLink href="https://www.linkedin.com/in/alex-rodriguez-45788b127/">LinkedIn</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="https://github.com/arodr856">GitHub</NavLink>
-                </NavItem>
-                <UncontrolledDropdown nav inNavbar>
-                    <DropdownToggle nav caret>
-                    Options
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                    <DropdownItem>
-                        Option 1
-                    </DropdownItem>
-                    <DropdownItem>
-                        Option 2
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>
-                        Reset
-                    </DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-                </Nav>
-            </Collapse>
+            <Navbar dark expand="md">
+                <NavbarBrand href="/">Alex Rodriguez</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="https://www.linkedin.com/in/alex-rodriguez-45788b127/">LinkedIn</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="https://github.com/arodr856">GitHub</NavLink>
+                    </NavItem>
+                    <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                        Options
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                        <DropdownItem>
+                            Option 1
+                        </DropdownItem>
+                        <DropdownItem>
+                            Option 2
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>
+                            Reset
+                        </DropdownItem>
+                        </DropdownMenu>
+                    </UncontrolledDropdown>
+                    </Nav>
+                </Collapse>
             </Navbar>
         </div>
         );
