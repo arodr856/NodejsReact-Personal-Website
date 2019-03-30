225be2d8c45d858f3a9a1c523dcb076f6f678d5a
arodr856 <arodr856@gmail.com>
2019-03-30 10:24:05 -0700

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 class App extends Component {
 
@@ -11,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar></NavBar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
+  
+export default class Example extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+        <div>
+            <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">Alex Rodriguez</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <NavLink href="https://www.linkedin.com/in/alex-rodriguez-45788b127/">LinkedIn</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="https://github.com/arodr856">GitHub</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                    Options
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                    <DropdownItem>
+                        Option 1
+                    </DropdownItem>
+                    <DropdownItem>
+                        Option 2
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                        Reset
+                    </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+                </Nav>
+            </Collapse>
+            </Navbar>
+        </div>
+        );
+    }
+}
+
