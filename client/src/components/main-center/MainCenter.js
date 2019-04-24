8cdb97f5d3c9aff9ae083daba6156e3a0bfa544d
arodr856 <arodr856@gmail.com>
2019-04-24 15:53:32 -0700

    deleting effect after word is typed is working

--- client/src/components/main-center/MainCenter.js
+++ client/src/components/main-center/MainCenter.js
@@ -4,10 +4,11 @@ class MainCenter extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            elements: ['Java', 'JavaScript', 'C', 'Node.js', 'Express', 'React', 'Redux', 'Web Developer'],
+            elements: ['Java', 'JavaScript', 'C.', 'Node.js', 'Express.js', 'React.js', 'Redux.js', 'Web Developer'],
             currentWord: '',
             currentIndex: 0,
             currentWordIndex: 0,
+            typing: true
         }
     }
 
@@ -15,20 +16,24 @@ class MainCenter extends React.Component {
 
         setInterval(async () => {
             const word = this.state.elements[this.state.currentIndex];
-            const index = this.state.currentIndex;
             const wordIndex = this.state.currentWordIndex;
             const currentWord = this.state.currentWord;
 
-            if(wordIndex < word.length){
+            if(wordIndex < word.length && this.state.typing){
                 await this.setState({currentWord: (currentWord + word.charAt(wordIndex)), currentWordIndex: (wordIndex + 1)});
             }else{  
-                await this.setState({currentIndex: ((this.state.currentIndex + 1) % this.state.elements.length), currentWordIndex: 0, currentWord: ''});
+                await this.setState({typing: false});
+                if(wordIndex > 0){
+                    await this.setState({currentWord: currentWord.substring(0, wordIndex), currentWordIndex: wordIndex - 1});
+                }else{
+                    await this.setState({typing: true});
+                    await this.setState({currentWord: '', currentIndex: (this.state.currentIndex + 1) % this.state.elements.length, wordIndex: 0});
+                }
             }
-        }, 1000);
+        }, 200);
         
     }
 
-    
     componentDidMount(){
         this.word();
     }
